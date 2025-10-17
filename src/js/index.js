68b39dddc02bbc453aa4ca6e41c9bba1bffdd98b
巽\(^o^)/~龍 <992640940@qq.com>
2025-10-17 22:37:33 +0800

    歌单推荐完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,37 @@
-const input = document.getElementById('J_input')
-const keyword = document.getElementById('J_keyword')
-console.log(input, keyword);
+function searchInput() {
+    const input = document.getElementById('J_input')
+    const keyword = document.getElementById('J_keyword')
 
-
-input.addEventListener('focus', () => {
-    keyword.classList.add('show')
-})
-input.addEventListener('blur', () => {
-    keyword.classList.remove('show')
-})
+    input.addEventListener('focus', () => {
+        keyword.classList.add('show')
+    })
+    input.addEventListener('blur', () => {
+        keyword.classList.remove('show')
+    })
+}
+function cardItemActive(){
+    const cardList = document.querySelectorAll('#J_card li')
+        const contentList = document.querySelectorAll('#J_music-content .music-content-item')
+        for (let i = 0; i < cardList.length; i++) {
+            cardList[i].addEventListener('mouseover', function () {
+                for (let j = 0; j < cardList.length; j++) {
+                    cardList[j].classList.remove('active')
+                    contentList[j].style.display = 'none'
+                }
+                contentList[i].style.display = 'block'
+                cardList[i].classList.add('active')
+            })  
+        }      
+        // cardList.forEach((item, index) => { 
+        //     item.addEventListener('mouseenter', () => { 
+        //         cardList.forEach((item, index) => { 
+        //             item.classList.remove('active')
+        //             contentList[index].style.display = 'none'
+        //         })
+        //         item.classList.add('active')
+        //         contentList[index].style.display = 'block'
+        //     })
+        // }) 
+}
+searchInput()
+cardItemActive()
