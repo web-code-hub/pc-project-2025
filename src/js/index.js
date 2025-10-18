ddd1997b4c55f1265a0e10a845b81483fb78f0f2
巽\(^o^)/~龍 <992640940@qq.com>
2025-10-18 13:33:49 +0800

    完成手机视频模块

--- src/js/index.js
+++ src/js/index.js
@@ -9,29 +9,48 @@ function searchInput() {
         keyword.classList.remove('show')
     })
 }
-function cardItemActive(){
+function cardItemActive() {
     const cardList = document.querySelectorAll('#J_card li')
-        const contentList = document.querySelectorAll('#J_music-content .music-content-item')
-        for (let i = 0; i < cardList.length; i++) {
-            cardList[i].addEventListener('mouseover', function () {
-                for (let j = 0; j < cardList.length; j++) {
-                    cardList[j].classList.remove('active')
-                    contentList[j].style.display = 'none'
-                }
-                contentList[i].style.display = 'block'
-                cardList[i].classList.add('active')
-            })  
-        }      
-        // cardList.forEach((item, index) => { 
-        //     item.addEventListener('mouseenter', () => { 
-        //         cardList.forEach((item, index) => { 
-        //             item.classList.remove('active')
-        //             contentList[index].style.display = 'none'
-        //         })
-        //         item.classList.add('active')
-        //         contentList[index].style.display = 'block'
-        //     })
-        // }) 
+    const contentList = document.querySelectorAll('#J_music-content .music-content-item')
+    for (let i = 0; i < cardList.length; i++) {
+        cardList[i].addEventListener('mouseover', function () {
+            for (let j = 0; j < cardList.length; j++) {
+                cardList[j].classList.remove('active')
+                contentList[j].style.display = 'none'
+            }
+            contentList[i].style.display = 'block'
+            cardList[i].classList.add('active')
+        })
+    }
+    // cardList.forEach((item, index) => { 
+    //     item.addEventListener('mouseenter', () => { 
+    //         cardList.forEach((item, index) => { 
+    //             item.classList.remove('active')
+    //             contentList[index].style.display = 'none'
+    //         })
+    //         item.classList.add('active')
+    //         contentList[index].style.display = 'block'
+    //     })
+    // }) 
+}
+
+function videoPlay() {
+    const videoList = document.querySelectorAll('.video-list li');
+    const videoMask = document.querySelector('.video-mask');
+    const video = document.querySelector('video');
+    const videoButton = document.querySelector('.video-button');
+    videoList.forEach(item => {
+        item.addEventListener('click', () => {
+            videoMask.style.display = 'block';
+            video.src = item.dataset.videoUrl
+            video.play()
+        })
+    })
+    videoButton.addEventListener('click', () => {
+        videoMask.style.display = 'none';
+        video.pause()
+    })
 }
 searchInput()
 cardItemActive()
+videoPlay()
